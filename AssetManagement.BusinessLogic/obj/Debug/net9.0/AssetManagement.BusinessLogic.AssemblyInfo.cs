3403f2fb6d49010450aa772cd80feb5356b2965d
David Chaplin <DaveC@wdm.co.uk>
2025-11-05 20:02:09 +0000

    changes

--- AssetManagement.BusinessLogic/obj/Debug/net9.0/AssetManagement.BusinessLogic.AssemblyInfo.cs
+++ AssetManagement.BusinessLogic/obj/Debug/net9.0/AssetManagement.BusinessLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AssetManagement.BusinessLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fd0c266b619a73a220b400fab521f701e559899")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bf6e77badde84528d7503e95d671ae48e43edc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AssetManagement.BusinessLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AssetManagement.BusinessLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
